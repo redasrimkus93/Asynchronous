87ab25aa23f0dfa703507a1aefe8118830083b13
Redas <redas.rimkus93@gmail.com>
2022-04-12 22:16:45 +0300

    initial commit

---
+++ Asynchronous/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Asynchronous
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Sync = new System.Windows.Forms.Button();
+            this.webInfo = new System.Windows.Forms.RichTextBox();
+            this.asyncButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Sync
+            // 
+            this.Sync.Location = new System.Drawing.Point(221, 34);
+            this.Sync.Name = "Sync";
+            this.Sync.Size = new System.Drawing.Size(172, 36);
+            this.Sync.TabIndex = 0;
+            this.Sync.Text = "SyncDownload";
+            this.Sync.UseVisualStyleBackColor = true;
+            this.Sync.Click += new System.EventHandler(this.Sync_Click);
+            // 
+            // webInfo
+            // 
+            this.webInfo.Location = new System.Drawing.Point(221, 109);
+            this.webInfo.Name = "webInfo";
+            this.webInfo.Size = new System.Drawing.Size(418, 226);
+            this.webInfo.TabIndex = 1;
+            this.webInfo.Text = "";
+            // 
+            // asyncButton
+            // 
+            this.asyncButton.Location = new System.Drawing.Point(458, 34);
+            this.asyncButton.Name = "asyncButton";
+            this.asyncButton.Size = new System.Drawing.Size(181, 36);
+            this.asyncButton.TabIndex = 2;
+            this.asyncButton.Text = "AsyncDownload";
+            this.asyncButton.UseVisualStyleBackColor = true;
+            this.asyncButton.Click += new System.EventHandler(this.asyncButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.asyncButton);
+            this.Controls.Add(this.webInfo);
+            this.Controls.Add(this.Sync);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Sync;
+        private System.Windows.Forms.RichTextBox webInfo;
+        private System.Windows.Forms.Button asyncButton;
+    }
+}
+
---
+++ Asynchronous/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asynchronous
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Sync_Click(object sender, EventArgs e)
+        {
+            RunDownloadSync();
+
+        }
+
+        private void RunDownloadSync()
+        {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+            List<string> webUrls =  PrepareData();
+            foreach (var url in webUrls)
+            {
+                var html = DownloadWebHtml(url);
+                AppendWebDataToTextBox(url, html);
+            }
+            stopwatch.Stop();
+            webInfo.Text += $"Elapsed time is {stopwatch.ElapsedMilliseconds}";
+        }
+
+        private async Task RunDownloadAsync()
+        {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+            List<string> webUrls = PrepareData();
+            List<Task<string>> downloadTasks = new();
+            foreach (var url in webUrls)
+            {
+                var htmlTask = DownloadWebHtmlAsync(url);
+                downloadTasks.Add(htmlTask);
+                //AppendWebDataToTextBox(url, htmlTask);
+            }
+
+            var websData = await Task.WhenAll(downloadTasks);
+
+            foreach (var item in websData)
+            {
+                AppendWebDataToTextBox(item, item);
+            }
+
+
+
+            stopwatch.Stop();
+            webInfo.Text += $"Elapsed time is {stopwatch.ElapsedMilliseconds}";
+        }
+
+        private void AppendWebDataToTextBox(string webName, string html)
+        {
+            webInfo.Text += $"Web has html length of {html.Length} {Environment.NewLine}";
+        }
+
+        private string DownloadWebHtml(string url)
+        {
+            WebClient webClient = new WebClient();
+            var html = webClient.DownloadString(url);
+            return html;
+        }
+
+        private async Task<string> DownloadWebHtmlAsync(string url)
+        {
+            WebClient webClient = new WebClient();
+            var html = await webClient.DownloadStringTaskAsync(url);
+
+            return html;
+        }
+
+        private List<string> PrepareData()
+        {
+            webInfo.Text = "";
+            List<string> webUrls = new();
+            webUrls.Add("https://www.microsoft.com/lt-lt");
+            webUrls.Add("https://docs.microsoft.com/en-us/learn/");
+            webUrls.Add("https://dotnet.microsoft.com/en-us/");
+            return webUrls;
+        }
+
+        private async void asyncButton_Click(object sender, EventArgs e)
+        {
+            await RunDownloadAsync();
+        }
+    }
+}
